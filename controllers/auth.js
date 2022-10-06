d3f2233120933d176ff8aabaffd331493756728f
Chris <chris.turcios@protonmail.com>
2022-10-05 22:40:50 -0500

    added mobile compatible styling and corrected signup form

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,8 +46,8 @@ exports.postLogin = (req, res, next) => {
 
 exports.logout = (req, res) => {
   req.logout(() => {
-    console.log('User has logged out.')
-  })
+    console.log("User has logged out.");
+  });
   req.session.destroy((err) => {
     if (err)
       console.log("Error : Failed to destroy the session during logout.", err);
@@ -110,7 +110,7 @@ exports.postSignup = (req, res, next) => {
           if (err) {
             return next(err);
           }
-          res.redirect("/select-restaurant");
+          res.redirect("/login");
         });
       });
     }
